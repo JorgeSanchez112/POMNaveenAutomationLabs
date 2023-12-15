dc8c5ce3c109ebfe9a068ac0218baa31782af7f8
Jorge Sanchez <halo3ratons@gmail.com>
2023-12-15 16:51:37 -0500

    DOCS: Fourth day

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -3,7 +3,9 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import util.TestUtil;
+import util.WebEventListener;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -14,6 +16,8 @@ public class TestBase {
 
     public static WebDriver driver;
     public static Properties prop;
+    public  static EventFiringWebDriver e_driver;
+    public static WebEventListener eventListener;
 
 
     public TestBase() {
@@ -36,6 +40,12 @@ public class TestBase {
             System.setProperty("webdriver.gecko.driver","C:\\Users\\Jorge\\Documents\\geckodriver.exe");
             driver = new FirefoxDriver();
         }
+
+        e_driver = new EventFiringWebDriver(driver);
+        // Now create object of EventListerHandler to register it with EventFiringWebDriver
+        eventListener = new WebEventListener();
+        e_driver.register(eventListener);
+        driver = e_driver;
 
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
---
+++ src/main/java/extentReportListener/ExtentReportNG.java
@@ -0,0 +1,70 @@
+package extentReportListener;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.testng.*;
+import org.testng.xml.XmlSuite;
+
+import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public class ExtentReportNG implements IReporter {
+    private ExtentReports extent;
+
+    public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
+
+        String path = outputDirectory + File.separator + "Extent.html";
+        System.out.println(path);
+        ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+        reporter.config().setReportName("Web Automation Results");
+        reporter.config().setDocumentTitle("Test Results");
+
+        extent = new ExtentReports();
+        extent.attachReporter(reporter);
+
+        for (ISuite suite : suites) {
+            Map<String, ISuiteResult> result = suite.getResults();
+
+            for (ISuiteResult r : result.values()) {
+                ITestContext context = r.getTestContext();
+
+                buildTestNodes(context.getPassedTests(), Status.PASS);
+                buildTestNodes(context.getFailedTests(), Status.FAIL);
+                buildTestNodes(context.getSkippedTests(), Status.SKIP);
+            }
+        }
+
+        extent.flush();
+    }
+
+    private void buildTestNodes(IResultMap tests, Status status) {
+        ExtentTest test;
+
+        if (tests.size() > 0) {
+            for (ITestResult result : tests.getAllResults()) {
+                test = extent.createTest(result.getMethod().getMethodName());
+
+                for (String group : result.getMethod().getGroups())
+                    test.assignCategory(group);
+
+                if (result.getThrowable() != null) {
+                    test.log(status, result.getThrowable());
+                } else {
+                    test.log(status, "Test " + status.toString().toLowerCase() + "ed");
+                }
+            }
+        }
+    }
+
+    private Date getTime(long millis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millis);
+        return calendar.getTime();
+    }
+
+}
